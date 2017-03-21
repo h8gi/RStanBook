1d6d5d180673bd8051cd5a111375bffc17e5596e
hiroki yagi <hiro.yagi0245@gmail.com>
2017-03-21 14:23:34 +0900

    exercise

--- chap04/exercise/generate-data.jl
+++ chap04/exercise/generate-data.jl
@@ -1,4 +1,4 @@
-using Distributions, StatPlots, DataFrames
+using Distributions, StatPlots, DataFrames, Stan
 N1 = 30
 N2 = 20
 Y1 = rand(Normal(0, 5), N1)
@@ -10,4 +10,15 @@ data2 = DataFrame(group=ones(Y2)+1, Y=Y2)
 boxplot(data1, :group,:Y)
 boxplot!(data2, :group,:Y)
 
-# ex2
+# ex2 
+# ex3: ./exercise3.stan
+stanmodel1 = Stanmodel(name="ex3", model=readstring("exercise3.stan"))
+data = [Dict("N1" => N1, "N2" => N2, "Y1" => Y1, "Y2" => Y2)]
+res1 = stan(stanmodel1, data)
+describe(res1)
+
+# ex4
+mu1sim = res1.value[:,findfirst(res1.names, "mu1"),:] |> vec
+mu2sim = res1.value[:,findfirst(res1.names, "mu2"),:] |> vec
+ex4prob = count(x->x, mu1sim .< mu2sim) / length(mu1sim)
+
